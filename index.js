9b0aefb84df183d9d7b91170f753439d2cd2d115
Amare AbebeE <amaremark@gmail.com>
2019-01-22 11:56:28 -0500

    Amare coomit

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const express = require('express')
-// Create a new instance of express 
+// const hbs = require("hbs")
 const app = express()
-// Creating an instance of mongoose 
 
-//Schema is connected to the mongoose 
-const Schema = mongoose.Schema
-
-const listSchema = new Schema({
-    name : String,
-    items : 
-})
-const listModel = mongoose.model("List", listSchema)
-//Insted of puting this here we put it in a folder called fouter 
-app.get('/home', (res,req) => {
-    listModel.find({}).then((results) => {
-        console.log(results)
-    })
-    res.send('hello form /home')
-})
-
+app.use(require('./routes/index'))
 app.listen(3000, () =>{
     console.log("I am listining")
 })
---
+++ routes/application.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const applicationController = require('../controllers/application.js')
+
+router.get("/ applicationController.index");
+module.exports = router
