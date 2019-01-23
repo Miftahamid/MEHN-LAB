448458dc4815f32252607434ab554554c8fb4f62
Amare AbebeE <amaremark@gmail.com>
2019-01-23 16:09:38 -0500

    new

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,32 +1,28 @@
 const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
-const Comment = new Schema({
+const recipe = new Schema({
   content: String,
+  ingrdients: String,
+  dairy: String,
+  vagitable: String,
+  fruits: String,
+  spices:Number,
+  condiments:String,
+  sauces: String,
+  baverages:String,
   createdAt: {
     type: Date,
     default: Date.now()
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
   }
 });
 
-const Recipe = new Schema({
-  content: String,
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
-  comments: [Comment]
-});
+   
+  
+
+ 
 
 module.exports = {
-  Recipe: mongoose.model("Recipe", Recipe),
-  Comment: mongoose.model("Comment", Comment)
+  recipe: mongoose.model("recipe", recipe),
+  // Comment: mongoose.model("Comment", Comment)
 };
